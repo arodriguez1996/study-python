07fb9ebc4e537a9ca4b7cc620160540e3cb146a1
arodriguez1996 <arodriguezv22@gmail.com>
2025-01-26 19:11:50 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Python study
+
+## Week 1
+
+- Hello world
+- Data types
+- Type convert
+- Variables
+- Data input
+- Numbers
+- Boolean
+- Strings
+- Exercises
---
+++ week1/01_print.py
@@ -0,0 +1,8 @@
+###
+# 01 - print()
+print('Hello world!!!!!!!!!')
+
+# 02 - print() with arguments, the default separator is a space but yout can change it with 'sep' argument
+print('hello', 'world', '!', sep=',', end=' ')
+# the 'end' argument is used to specify what to print at the end of the line, by default is '\n'
+print('otra linea')
